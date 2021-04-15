ca9dbca343ee792eb6f32c840294549a5586aa17
Carson-Key <keyc@oregonstate.edu>
2021-04-15 10:44:09 -0700

    Add new line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -3,7 +3,8 @@ class House
         "the malt that lay in ", 
         "the rat that ate the malt that lay in ",
         "the cat that killed the rat that ate the malt that lay in ",
-        "the dog that worried the cat that killed the rat that ate the malt that lay in "
+        "the dog that worried the cat that killed the rat that ate the malt that lay in ",
+        "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     ]
 
     def line(number)
