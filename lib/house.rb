acf2a972a5de57b8a5d45959f6464ccf161cc37d
Carson-Key <keyc@oregonstate.edu>
2021-04-15 10:46:45 -0700

    Add new line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -5,7 +5,8 @@ class House
         "the cat that killed the rat that ate the malt that lay in ",
         "the dog that worried the cat that killed the rat that ate the malt that lay in ",
         "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-        "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
+        "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     ]
 
     def line(number)
