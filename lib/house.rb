9ff908582fe2a398b00cbca69e574a5f01bf05b9
Carson-Key <keyc@oregonstate.edu>
2021-04-15 10:49:25 -0700

    Add new line to lines

--- lib/house.rb
+++ lib/house.rb
@@ -7,7 +7,8 @@ class House
         "the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
         "the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
         "the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
-        "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+        "the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in ",
+        "the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
     ]
 
     def line(number)
