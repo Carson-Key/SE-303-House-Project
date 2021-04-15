1331f2e178d6642b20bee51ca1d92ff5e61d87c0
Carson-Key <keyc@oregonstate.edu>
2021-04-15 10:40:52 -0700

    Extract duplication to array

--- lib/house.rb
+++ lib/house.rb
@@ -1,10 +1,12 @@
 class House
+    @@lines = ["", "", "the malt that lay in "]
+
     def line(number)
         case number
         when 1
-            "This is the house that Jack built.\n"
+            "This is #{@@lines[number]}the house that Jack built.\n"
         when 2
-            "This is the malt that lay in the house that Jack built.\n"
+            "This is #{@@lines[number]}the house that Jack built.\n"
         when 3
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
         when 4
