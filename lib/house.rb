97bedd677cd377e0b5c258412ac18f15459ab824
Carson-Key <keyc@oregonstate.edu>
2021-04-15 10:31:25 -0700

    Transform conditional to switch

--- lib/house.rb
+++ lib/house.rb
@@ -1,8 +1,9 @@
 class House
     def line(number)
-        if number == 1
+        case number
+        when 1
             "This is the house that Jack built.\n"
-        elsif number ==  2
+        when 2
             "This is the malt that lay in the house that Jack built.\n"
         else
             "This is the rat that ate the malt that lay in the house that Jack built.\n"
