36773b09085b76cb46745b6704c96975e783d933
Carson-Key <keyc@oregonstate.edu>
2021-05-26 15:00:33 -0700

    Reverse previous commit

--- lib/house.rb
+++ lib/house.rb
@@ -22,7 +22,7 @@ class House
     def phrase(number)
         case number
         when 2
-            phrases.first(number - 1).join()
+            "the malt that lay in "
         when 3
             "the rat that ate the malt that lay in "
         when 4
