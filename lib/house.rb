d3a2ae4969fc9fe49d38082d5885cda4b3f93c6e
Carson-Key <keyc@oregonstate.edu>
2021-05-26 15:24:46 -0700

    Remove case 4 of phrase, duplicate return

--- lib/house.rb
+++ lib/house.rb
@@ -21,8 +21,6 @@ class House
 
     def phrase(number)
         case number
-        when 4
-            phrases.first(number-1).reverse.join()
         when 5
             "#{phrases[3]}#{phrases[2]}#{phrases[1]}#{phrases[0]}"
         when 6
