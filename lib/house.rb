677f8bf03926ca00850d2ba89ac75223d3af4642
Carson-Key <keyc@oregonstate.edu>
2021-05-26 15:16:31 -0700

    Add repeat phrases to case 11 of phrase method

--- lib/house.rb
+++ lib/house.rb
@@ -40,7 +40,7 @@ class House
         when 10
             "#{phrases[8]}#{phrases[7]}#{phrases[6]}#{phrases[5]}#{phrases[4]}#{phrases[3]}#{phrases[2]}#{phrases[1]}#{phrases[0]}"
         when 11
-            "the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
+            "the farmer sowing his corn that kept #{phrases[8]}#{phrases[7]}#{phrases[6]}#{phrases[5]}#{phrases[4]}#{phrases[3]}#{phrases[2]}#{phrases[1]}#{phrases[0]}"
         when 12
             "the horse and the hound and the horn that belonged to the farmer sowing his corn that kept the rooster that crowed in the morn that woke the priest all shaven and shorn that married the man all tattered and torn that kissed the maiden all forlorn that milked the cow with the crumpled horn that tossed the dog that worried the cat that killed the rat that ate the malt that lay in "
         else
